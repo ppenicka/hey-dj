f883f8a8b580ba75eb0862a19f05284043d7c1af
Petr Penicka <petr.penicka@gmail.com>
2020-04-17 16:11:56 +0200

    feat: 3rd party links working

--- client/src/Components/TrackTile/TrackTile.js
+++ client/src/Components/TrackTile/TrackTile.js
@@ -12,22 +12,22 @@ export default (props) => (
     }
     {
       (props.track.metadata && props.track.metadata.music[0].external_metadata.musicbrainz) ?
-      <img className="Logo" src={`${process.env.PUBLIC_URL}/musicbrainz_monochrome_hexagon.png`} alt="Info on MusicBrainz"></img>
+      <a href={`https://musicbrainz.org/recording/${props.track.metadata.music[0].external_metadata.musicbrainz[0].track.id}`} target="_blank"><img className="Logo" src={`${process.env.PUBLIC_URL}/musicbrainz_monochrome_hexagon.png`} alt="Info on MusicBrainz"></img></a>
         : <img className="LogoHidden" src={`${process.env.PUBLIC_URL}/musicbrainz_monochrome_hexagon.png`} alt="Info on MusicBrainz"></img>
     }
     {
       (props.track.metadata && props.track.metadata.music[0].external_metadata.deezer) ?
-      <img className="Logo" src={`${process.env.PUBLIC_URL}/deezer_round_monochrome.png`} alt="Listen on Deezer"></img>
+      <a href={`https://deezer.com/track/${props.track.metadata.music[0].external_metadata.deezer.track.id}`} target="_blank"><img className="Logo" src={`${process.env.PUBLIC_URL}/deezer_round_monochrome.png`} alt="Listen on Deezer"></img></a>
         : <img className="LogoHidden" src={`${process.env.PUBLIC_URL}/deezer_round_monochrome.png`} alt="Listen on Deezer"></img>
     }
     {
       (props.track.metadata && props.track.metadata.music[0].external_metadata.youtube) ?
-      <img className="Logo" src={`${process.env.PUBLIC_URL}/yt_round_monochrome.png`} alt="Listen on YouTube"></img>
+      <a href={`https://www.youtube.com/watch?v=${props.track.metadata.music[0].external_metadata.youtube.vid}`} target="_blank"><img className="Logo" src={`${process.env.PUBLIC_URL}/yt_round_monochrome.png`} alt="Listen on YouTube"></img></a>
         : <img className="LogoHidden" src={`${process.env.PUBLIC_URL}/yt_round_monochrome.png`} alt="Listen on YouTube"></img>
     }
     {
       (props.track.metadata && props.track.metadata.music[0].external_metadata.spotify) ?
-      <img className="Logo" src={`${process.env.PUBLIC_URL}/Spotify_Icon_RGB_Black.png`} alt="Listen on Spotify"></img>
+      <a href={`http://open.spotify.com/track/${props.track.metadata.music[0].external_metadata.spotify.track.id}`} target="_blank"><img className="Logo" src={`${process.env.PUBLIC_URL}/Spotify_Icon_RGB_Black.png`} alt="Listen on Spotify"></img></a>
         : <img className="LogoHidden" src={`${process.env.PUBLIC_URL}/Spotify_Icon_RGB_Black.png`} alt="Listen on Spotify"></img>
     }
   </div>
