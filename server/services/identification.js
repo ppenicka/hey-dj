63ad8de42cee961abe2083c4cfc078692b1753c3
Petr Penicka <petr.penicka@gmail.com>
2020-04-18 19:25:31 +0200

    fix: using status codes instead of status messages

--- server/services/identification.js
+++ server/services/identification.js
@@ -30,7 +30,7 @@ function getTracklist (req, res) {
         // second try for unidentified
         Promise.all(results).then((results) => {
           for (let i = 0; i < segments; i++) {
-            if (results[i].status.msg === 'No result') {
+            if (results[i].status.code === 1001) {
               results[i] = identifySegment(source, 60 + i * interval + Math.floor(interval / 2), 72 + i * interval + Math.floor(interval / 2), `${dirname}/${i}.mp3`);
             }
           }
@@ -39,8 +39,8 @@ function getTracklist (req, res) {
           Promise.all(results).then((results) => {
             let i = 0;
             while (i < segments - 1) {
-              if ((results[i].status.msg !== 'Success') && (results[i + 1].status.msg !== 'Success') ||
-                ((results[i].status.msg === 'Success') && (results[i + 1].status.msg === 'Success') &&
+              if ((results[i].status.code !== 0) && (results[i + 1].status.code !== 0) ||
+                  ((results[i].status.code === 0) && (results[i + 1].status.code === 0) &&
                   (results[i].metadata.music[0].title === results[i + 1].metadata.music[0].title) &&
                   (results[i].metadata.music[0].duration === results[i + 1].metadata.music[0].duration))) {
                 results.splice(i + 1, 1);
@@ -50,7 +50,7 @@ function getTracklist (req, res) {
 
             console.log('######## Identified tracklist: ########');
             for (let i = 0; i < segments; i++) {
-              if (results[i].status.msg === 'Success') {
+              if (results[i].status.code === 0) {
                 console.log(`Track #${i + 1}: ${results[i].metadata.music[0].artists[0].name} - ${results[i].metadata.music[0].title}`);
               } else {
                 console.log(`Track #${i + 1}: unidentified`);
@@ -74,6 +74,7 @@ function identifySegment (source, start, end, output) {
     source.then(function (audio) {                      // ffmpeg promise from source file
       audio.addCommand('-ss', `${start}`);              // start time of segment
       audio.addCommand('-to', `${end}`);                // end time of segment
+
       audio.save(`${output}`).then(() => {              // save segment to file
         console.log(`Saved segment to file: ${output}`);
         return requestMetadata(output);                 // get metadata from API
