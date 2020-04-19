3cab8e4d8f68ecb5b42c1cebda51f264e8ea4751
Petr Penicka <petr.penicka@gmail.com>
2020-04-19 15:13:07 +0200

    feat: youtube identification route working (lots of duplicate code)

--- server/router.js
+++ server/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const identify = require('./services/identification');
+const yt = require('./services/ytdownload');
 
 router
   .post('/tracklist', identify.getTracklist);
+
+router
+  .get('/yt', yt.downloadYouTube)
 
 module.exports = router;
