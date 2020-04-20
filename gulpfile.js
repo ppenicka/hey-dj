a59fa3bf9481cdd0063f8bb3eece471729457187
Petr Penicka <petr.penicka@gmail.com>
2020-04-20 22:32:34 +0200

    chore: codeworks-style gulp and eslint setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  console.log('test')
-  cb();
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+
+// Utility to ignore unnecessary files
+// when generating the glob patterns array for gulp.src()
+function addDefSrcIgnore (srcArr) {
+  return srcArr.concat([
+    '!node_modules{,/**}',
+    '!.git{,/**}',
+    '!**/node_modules{,/**}'
+  ]);
 }
 
-exports.default = defaultTask
+// JavaScript and JSON linter
+function lint () {
+  return gulp
+    .src(addDefSrcIgnore(['**/*.js', '**/*.json']), { dot: true })
+    .pipe(eslint({ dotfiles: true }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Lint all files
+exports.lint = lint;
