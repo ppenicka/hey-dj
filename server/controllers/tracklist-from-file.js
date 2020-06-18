de1d683f9fb307ee7d9b431c50cb7f7c0e868432
Petr Penicka <petr.penicka@gmail.com>
2020-06-19 01:37:56 +0200

    refactor: cleaner initialization of temporary file name variables

--- server/controllers/tracklist-from-file.js
+++ server/controllers/tracklist-from-file.js
@@ -6,21 +6,23 @@ function identifyFromFile (req, res) {
   const interval = process.env.SEGMENT_INTERVAL || 120;
   const name = req.files.file.name;
   const size = req.files.file.size;
-  const extension = name.substring(name.length - 3, name.length);
-  const id = shortid.generate();
-  const input = './tmp/' + id + '.' + extension;
-  const dirname = input.substring(0, input.length - 4);
 
   TracklistFromFile.find({ fileName: name, fileSize: size }).then((cached) => {
     if (cached.length > 0) {
       res.status(200).send(cached[0]['tracklist']);
     } else {
+      const extension = name.substring(name.length - 3, name.length);
+      const id = shortid.generate();
+      const dirname = './tmp/' + id;
+      const input = dirname + extension;
+
       req.files.file.mv(input).then(() => {
         getTracklist(input, dirname, extension, interval).then((results) => {
           TracklistFromFile.create({
             fileName: name,
             fileSize: size,
-            tracklist: JSON.stringify(results) });
+            tracklist: JSON.stringify(results)
+          });
           res.status(200).send(results);
         });
       });
