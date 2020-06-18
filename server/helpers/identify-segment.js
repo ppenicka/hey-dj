69c65a2036f8bcbb07637f321708d0393970499b
Petr Penicka <petr.penicka@gmail.com>
2020-06-19 00:38:06 +0200

    fix: added handling of promise rejection in identifySegment

--- server/helpers/identify-segment.js
+++ server/helpers/identify-segment.js
@@ -3,19 +3,23 @@ const fs = require('fs');
 
 function identifySegment (source, start, end, output) {
   return new Promise((resolve, reject) => {
-    source.then(function (audio) {                        // ffmpeg promise from source file
-      audio.addCommand('-ss', `${start}`);                // start time of segment
-      audio.addCommand('-to', `${end}`);                  // end time of segment
+    try {
+      source.then(function (audio) {                        // ffmpeg promise from source file
+        audio.addCommand('-ss', `${start}`);                // start time of segment
+        audio.addCommand('-to', `${end}`);                  // end time of segment
 
-      audio.save(`${output}`).then(() => {                // save segment to file
-        console.log(`Saved segment to file: ${output}`);                // eslint-disable-line no-console
-        return requestMetadata(output);                   // get metadata from API
-      }).then((trackMetadata) => {
-        console.log(`Received identification response for ${output}`);  // eslint-disable-line no-console
-        fs.unlink(`${output}`, () => true);               // delete segment file
-        resolve(trackMetadata);                           // resolve promise with metadata
+        audio.save(`${output}`).then(() => {                // save segment to file
+          console.log(`Saved segment to file: ${output}`);                // eslint-disable-line no-console
+          return requestMetadata(output);                   // get metadata from API
+        }).then((trackMetadata) => {
+          console.log(`Received identification response for ${output}`);  // eslint-disable-line no-console
+          fs.unlink(`${output}`, () => true);               // delete segment file
+          resolve(trackMetadata);                           // resolve promise with metadata
+        });
       });
-    });
+    } catch (e) {
+      console.log(`Identification of segment at second ${start} failed with error message: ${e}`);
+    }
   });
 }
 
